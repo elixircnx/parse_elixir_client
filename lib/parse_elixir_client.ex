6e3493e8d4db0c5a5bd4ff03a9badce8797f4e19
David Whitlock <alovedalongthe@gmail.com>
2014-08-24 11:29:17 +0700

    Added requests module

--- lib/parse_elixir_client.ex
+++ lib/parse_elixir_client.ex
@@ -27,62 +27,22 @@ defmodule ParseClient do
       ParseClient.delete("classes/Animals/12345678")
   """
 
-  use HTTPoison.Base
-
-  @parse_url "https://api.parse.com/1/"
-
-  @doc """
-  Creates the URL for an endpoint
-  Args:
-    * endpoint - part of the API we're hitting
-  Returns string
-
-  ## Example
-      iex> endpoint = "classes/GameScore"
-      iex> ParseClient.process_url(endpoint)
-      "https://api.parse.com/1/classes/GameScore"
-  """
-  def process_url(endpoint) do
-    @parse_url <> endpoint
-  end
-
-  @doc """
-  Checks that the body can be encoded and handles any errors
-  ## Example
-      iex> body = %{"job" => "Lumberjack", "clothes" => "stockings"}
-      iex> ParseClient.process_request_body(body)
-      ~S({\"clothes\":\"stockings\",\"job\":\"Lumberjack\"})
-  """
-  def process_request_body(body), do: JSEX.encode! body
-
-  @doc """
-  Checks that the body can be decoded and handles any errors
-  Converts binary response keys to atoms
-  Args:
-  * body - string binary response
-  Returns Record or ArgumentError
-  ## Example
-      iex> body = ~S({\"score\":1337,\"objectId\":\"sOxpug2373\",\"playerName\":\"Sean Plott\"})
-      iex> ParseClient.process_response_body(body)
-      %{score: 1337, objectId: "sOxpug2373", playerName: "Sean Plott"}
-  """
-  def process_response_body(body) do
-    case JSEX.decode(body, [{:labels, :atom}]) do
-      {:ok, text} -> text
-      {:error, _} -> "An error has occurred while processing the json response"
-    end
-  end
+  alias ParseClient.Requests
 
   @doc """
   Parse filters and options in the query.
   """
+  def parse_filters(filters, options) when is_map(filters) and is_map(options) do
+    %{where: JSEX.encode!(filters)} |> URI.encode_query
+  end
+
   def parse_filters(filters, options) do
     unless filters == "", do: filters = %{where: JSEX.encode!(filters)} |> URI.encode_query
-    unless options == %{}, do: options = options |> URI.encode_query
+    unless options == "", do: options = options |> URI.encode_query
     filters <> options
   end
 
-  def get(url), do: request(:get, url, "", get_headers)
+  def get(url), do: Requests.request(:get, url, "", get_headers)
 
   @doc """
   Get request with filters.
@@ -94,18 +54,18 @@ defmodule ParseClient do
 
       ParseClient.get("classes/Animals", "", %{"order" => "createdAt"})
   """
-  def get(url, filters, options \\ %{}) do
+  def get(url, filters, options \\ "") do
     filter_string = parse_filters(filters, options)
-    request(:get, url <> "?" <> filter_string, "", get_headers)
+    Requests.request(:get, url <> "?" <> filter_string, "", get_headers)
   end
 
-  def post(url, body), do: request(:post, url, body, post_headers)
+  def post(url, body), do: Requests.request(:post, url, body, post_headers)
 
-  def put(url, body), do: request(:put, url, body, post_headers)
+  def put(url, body), do: Requests.request(:put, url, body, post_headers)
 
-  def delete(url), do: request(:delete, url, "", get_headers)
+  def delete(url), do: Requests.request(:delete, url, "", get_headers)
 
-  def query(args), do: get(args).body
+  def query(url), do: get(url).body
 
   def query(url, filters, options \\ []), do: get(url, filters, options).body
 
