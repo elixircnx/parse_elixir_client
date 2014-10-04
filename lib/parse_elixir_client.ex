cea526222c9a6252e016cf7fcddc632aade923b5
David Whitlock <alovedalongthe@gmail.com>
2014-10-04 18:45:27 +0700

    Added user and file functions

--- lib/parse_elixir_client.ex
+++ lib/parse_elixir_client.ex
@@ -52,4 +52,122 @@ defmodule ParseClient do
 
       ParseClient.query("classes/Animals", %{"name" => %{"$exists" => true}, "status" => 1})
   """
+
+  alias ParseClient.Requests, as: Req
+  alias ParseClient.Authenticate, as: Auth
+
+  @doc """
+  Get request for making queries.
+  """
+  def get(url), do: Req.request(:get, url, "", get_headers)
+
+  @doc """
+  Get request with filters.
+
+  ## Examples
+
+  To make a query just about animals that have a name and are still alive:
+
+      ParseClient.get("classes/Animals", %{"name" => %{"$exists" => true}, "status" => 1})
+
+  To make a request with options, but no filters, use %{} as the second argument:
+
+      ParseClient.get("classes/Animals", %{}, %{"order" => "createdAt"})
+  """
+  def get(url, filters, options \\ %{}) do
+    filter_string = Req.parse_filters(filters, options)
+    Req.request :get, url <> "?" <> filter_string, "", get_headers
+  end
+
+  @doc """
+  Get request for making queries. Just returns the body of the response.
+  """
+  def query(url), do: get(url).body
+
+  @doc """
+  Get request for making queries with filters and options.
+  Just returns the body of the response.
+  """
+  def query(url, filters, options \\ %{}) do
+    get(url, filters, options).body
+  end
+
+  @doc """
+  Request to create an object.
+
+  ## Example
+
+      body = %{"animal" => "parrot, "name" => "NorwegianBlue", "status" => 0}
+      ParseClient.post("classes/Animals", body)
+
+  """
+  def post(url, body), do: Req.request(:post, url, body, post_headers)
+
+  @doc """
+  Request to update an object.
+
+  ## Example
+
+      ParseClient.put("classes/Animals/12345678", %{"status" => 1})
+
+  """
+  def put(url, body), do: Req.request(:put, url, body, post_headers)
+
+  @doc """
+  Request to delete an object.
+
+  ## Example
+
+      ParseClient.delete("classes/Animals/12345678")
+
+  """
+  def delete(url), do: Req.request(:delete, url, "", get_headers)
+
+  @doc """
+  Requests from a user to signup. The user must provide a username
+  and a password. The options argument refers to additional information,
+  such as email address or phone number, and it needs to be in the form
+  of an Elixir map.
+  """
+  def signup(username, password, options \\ %{}) do
+    data = Dict.merge(%{"username" => username, "password" => password}, options)
+    post "users", data
+  end
+
+  @doc """
+  Request from a user to login. Username and password are required.
+  """
+  def login(username, password) do
+    get "login", %{"username" => username, "password" => password}
+  end
+
+  @doc """
+  Request to reset a user's password.
+  """
+  def request_passwd_reset(email) do
+    post "requestPasswordReset", %{"email" => email}
+  end
+
+  @doc """
+  Validates the user. Takes the user's session token as the only argument.
+  """
+  def validate_user(token_val) do
+    Req.request :get, "users/me", "", post_headers("X-Parse-Session-Token", token_val)
+  end
+
+  @doc """
+  Post request to upload a file.
+  """
+  def upload_file(url, contents, content_type) do
+    Req.request :post, url, contents, post_headers("Content-Type", content_type)
+  end
+
+  defp get_headers do
+    %{"X-Parse-Application-Id" => Auth.config_parse_id,
+      "X-Parse-REST-API-Key"   => Auth.config_parse_key}
+  end
+
+  defp post_headers(key \\ "Content-Type", val \\ "application/json") do
+    Dict.put(get_headers, key, val)
+  end
 end
--- lib/parse_elixir_client/requests.ex
+++ lib/parse_elixir_client/requests.ex
@@ -6,7 +6,6 @@ defmodule ParseClient.Requests do
   """
 
   use HTTPoison.Base
-  alias ParseClient.Authenticate, as: Auth
 
   @parse_url "https://api.parse.com/1/"
 
@@ -70,94 +69,5 @@ defmodule ParseClient.Requests do
 
   def parse_filters(_, _) do
     raise ArgumentError, message: "filters and options arguments should be maps"
-  end
-
-  @doc """
-  Get request for making queries.
-  """
-  def get(url), do: request(:get, url, "", get_headers)
-
-  @doc """
-  Get request with filters.
-
-  ## Examples
-
-  To make a query just about animals that have a name and are still alive:
-
-      ParseClient.get("classes/Animals", %{"name" => %{"$exists" => true}, "status" => 1})
-
-  To make a request with options, but no filters, use %{} as the second argument:
-
-      ParseClient.get("classes/Animals", %{}, %{"order" => "createdAt"})
-  """
-  def get(url, filters, options \\ %{}) do
-    filter_string = parse_filters(filters, options)
-    request :get, url <> "?" <> filter_string, "", get_headers
-  end
-
-  @doc """
-  Get request for making queries. Just returns the body of the response.
-  """
-  def query(url), do: get(url).body
-
-  @doc """
-  Get request for making queries with filters and options.
-  Just returns the body of the response.
-  """
-  def query(url, filters, options \\ %{}) do
-    get(url, filters, options).body
-  end
-
-  @doc """
-  Request to create an object.
-
-  ## Example
-
-      body = %{"animal" => "parrot, "name" => "NorwegianBlue", "status" => 0}
-      ParseClient.post("classes/Animals", body)
-
-  """
-  def post(url, body), do: request(:post, url, body, post_headers)
-
-  @doc """
-  Request to update an object.
-
-  ## Example
-
-      ParseClient.put("classes/Animals/12345678", %{"status" => 1})
-
-  """
-  def put(url, body), do: request(:put, url, body, post_headers)
-
-  @doc """
-  Request to delete an object.
-
-  ## Example
-
-      ParseClient.delete("classes/Animals/12345678")
-
-  """
-  def delete(url), do: request(:delete, url, "", get_headers)
-
-  @doc """
-  """
-  def validate(url, token_key, token_val) do
-    request :get, url, "", post_headers(token_key, token_val)
-  end
-
-  @doc """
-  Post request to upload a file.
-  """
-  def upload_file(url, contents, content_type) do
-    request :post, url, contents, post_headers("Content-Type", content_type)
-  end
-
-  defp get_headers do
-    %{"X-Parse-Application-Id" => Auth.config_parse_id,
-      "X-Parse-REST-API-Key"   => Auth.config_parse_key}
-  end
-
-  defp post_headers(key \\ "Content-Type", val \\ "application/json") do
-    Dict.put(get_headers, key, val)
   end
 end
