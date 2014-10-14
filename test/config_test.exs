72eb37193d852982266e14ed85911f6548511c96
mutablestate <benwebdev@gmail.com>
2014-10-15 04:25:25 +0700

    replace nil with system variables for default config

--- test/config_test.exs
+++ test/config_test.exs
@@ -6,18 +6,17 @@ defmodule ParseClient.ConfigTest do
   test "returns parse_application_id" do
     Application.put_env(:parse_client, :parse_application_id, "123")
     assert config_parse_id == "123"
-
-    Application.put_env(:parse_client, :parse_application_id, nil)
   end
 
   test "returns parse_api_key" do
     Application.put_env(:parse_client, :parse_api_key, "456")
     assert config_parse_key == "456"
-
-    Application.put_env(:parse_client, :parse_api_key, nil)
   end
 
-  test "returns ArgumentError when environment variable is nil" do
+  test "returns ArgumentError when environment variables are not set" do
+    Application.put_env(:parse_client, :parse_api_key, nil)
+    Application.put_env(:parse_client, :parse_application_id, nil)
+
     assert_raise ArgumentError, "Authentication failed. Add API key and App ID to config files.", fn ->
       config_parse_id
     end
